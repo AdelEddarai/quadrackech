24e26a5eb3a86557f57c422ff512483c8aa026fc
Adel Eddarai <96699224+AdelEddarai@users.noreply.github.com>
2023-07-26 17:10:28 +0200

    Add files via upload

---
+++ destinations/360-image-preview-main/360-image-preview-main/assets/js/360.js
@@ -0,0 +1,266 @@
+import * as THREE from './three.module.min.js';
+
+let camera, scene, renderer;
+
+let isUserInteracting = false,
+    onPointerDownMouseX = 0, onPointerDownMouseY = 0,
+    lon = -56, onPointerDownLon = 0,
+    lat = -20, onPointerDownLat = 0,
+    phi = 0, theta = 0;
+
+const onProgressContainer = document.getElementById('onProgressContainer');
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   console.log('loaded');
+//   loadingImage();
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log('loaded');
+
+    loadingImage();
+});
+
+function loadingImage () {
+    const manager = new THREE.LoadingManager();
+    manager.onStart = function (url, itemsLoaded, itemsTotal) {
+
+        console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+
+    };
+
+    manager.onLoad = function () {
+
+        console.log('Loading complete!');
+        onProgressContainer.classList.add('hide');
+
+    };
+
+
+    manager.onProgress = function (url, itemsLoaded, itemsTotal) {
+
+        console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+
+    };
+
+    manager.onError = function (url) {
+
+        console.log('There was an error loading ' + url);
+
+    };
+
+    const loader = new THREE.TextureLoader(manager);
+    loader.load('./assets/images/panoramaMain.PNG', function (texture) {
+
+        const geometry = new THREE.SphereGeometry(500, 60, 40);
+    // invert the geometry on the x-axis so that all of the faces point inward
+        geometry.scale(- 1, 1, 1);
+        
+        const texture2 = texture;
+        texture2.mapping = THREE.EquirectangularReflectionMapping;
+
+        // const texture = new THREE.TextureLoader().load('/assets/images/panoramaMain.PNG');
+        const material = new THREE.MeshBasicMaterial({ map: texture2 });
+
+        const mesh = new THREE.Mesh(geometry, material);
+
+        scene.add(mesh);
+
+    });
+}
+
+
+init();
+animate();
+
+function init() {
+
+    const container = document.getElementById('container');
+
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 1100);
+
+    scene = new THREE.Scene();
+
+    // const geometry = new THREE.SphereGeometry(500, 60, 40);
+    // // invert the geometry on the x-axis so that all of the faces point inward
+    // geometry.scale(- 1, 1, 1);
+
+
+    // const manager = new THREE.LoadingManager();
+    // manager.onStart = function (url, itemsLoaded, itemsTotal) {
+
+    //     console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+
+    // };
+
+    // manager.onLoad = function () {
+
+    //     console.log('Loading complete!');
+
+    // };
+
+
+    // manager.onProgress = function (url, itemsLoaded, itemsTotal) {
+
+    //     console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+
+    // };
+
+    // manager.onError = function (url) {
+
+    //     console.log('There was an error loading ' + url);
+
+    // };
+
+    // const loader = new THREE.TextureLoader(manager);
+    // loader.load('./assets/images/panoramaMain.PNG', function (texture) {
+
+    //     // const texture = new THREE.TextureLoader().load('/assets/images/panoramaMain.PNG');
+    //     const material = new THREE.MeshBasicMaterial({ map: texture });
+
+    //     const mesh = new THREE.Mesh(geometry, material);
+
+    //     scene.add(mesh);
+
+    // });
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    container.style.touchAction = 'none';
+    container.addEventListener('pointerdown', onPointerDown);
+
+    document.addEventListener('wheel', onDocumentMouseWheel);
+
+    //
+
+    document.addEventListener('dragover', function (event) {
+
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'copy';
+
+    });
+
+    document.addEventListener('dragenter', function () {
+
+        document.body.style.opacity = 0.5;
+
+    });
+
+    document.addEventListener('dragleave', function () {
+
+        document.body.style.opacity = 1;
+
+    });
+
+    // document.addEventListener('drop', function (event) {
+
+    //     event.preventDefault();
+
+    //     const reader = new FileReader();
+    //     reader.addEventListener('load', function (event) {
+
+    //         material.map.image.src = event.target.result;
+    //         material.map.needsUpdate = true;
+
+    //     });
+    //     reader.readAsDataURL(event.dataTransfer.files[0]);
+
+    //     document.body.style.opacity = 1;
+
+    // });
+
+    //
+
+    window.addEventListener('resize', onWindowResize);
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function onPointerDown(event) {
+
+    if (event.isPrimary === false) return;
+
+    isUserInteracting = true;
+
+    onPointerDownMouseX = event.clientX;
+    onPointerDownMouseY = event.clientY;
+
+    onPointerDownLon = lon;
+    onPointerDownLat = lat;
+
+    document.addEventListener('pointermove', onPointerMove);
+    document.addEventListener('pointerup', onPointerUp);
+
+}
+
+function onPointerMove(event) {
+
+    if (event.isPrimary === false) return;
+
+    lon = (onPointerDownMouseX - event.clientX) * 0.1 + onPointerDownLon;
+    lat = (event.clientY - onPointerDownMouseY) * 0.1 + onPointerDownLat;
+
+}
+
+function onPointerUp() {
+
+    if (event.isPrimary === false) return;
+
+    isUserInteracting = false;
+
+    document.removeEventListener('pointermove', onPointerMove);
+    document.removeEventListener('pointerup', onPointerUp);
+
+}
+
+function onDocumentMouseWheel(event) {
+
+    const fov = camera.fov + event.deltaY * 0.05;
+
+    camera.fov = THREE.MathUtils.clamp(fov, 30, 90);
+
+    camera.updateProjectionMatrix();
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+    update();
+
+}
+
+function update() {
+
+    // if (isUserInteracting === false) {
+
+    //     lon += 0.1;
+
+    // }
+
+    lat = Math.max(- 85, Math.min(85, lat));
+    phi = THREE.MathUtils.degToRad(90 - lat);
+    theta = THREE.MathUtils.degToRad(lon);
+
+    const x = 500 * Math.sin(phi) * Math.cos(theta);
+    const y = 500 * Math.cos(phi);
+    const z = 500 * Math.sin(phi) * Math.sin(theta);
+
+    camera.lookAt(x, y, z);
+
+    renderer.render(scene, camera);
+
+}
+
